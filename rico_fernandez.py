b5cb6158fcf8855b7e7378ada8ddb5705c3c139d
RESTAURATEBEF <efrainfernandezboza99@gmail.com>
2026-08-13 17:47:10 -0500

    Update rico_fernandez.py

--- rico_fernandez.py
+++ rico_fernandez.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 # 1. Configuración de la página
 st.set_page_config(
-    page_title="RESTAURANT RICO FERNANDEZ - Menú Digital",
+    page_title="RESTAURANT FERNANDEZ - Menú Digital",
     page_icon="🍔",
     layout="centered",
     initial_sidebar_state="collapsed",
